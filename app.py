c62a18925617aa20003bb21e3895728841957111
Rupam Patra <patra.ru@northeastern.edu>
2024-12-19 06:51:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-import spacy
 import os
 import spacy
 import subprocess
@@ -14,15 +13,14 @@ import subprocess
 # Ensure spaCy model is installed and loaded
 def load_spacy_model():
     try:
-        # Try to load the model
         return spacy.load("en_core_web_sm")
     except OSError:
-        # If the model is not installed, install it
         subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
         return spacy.load("en_core_web_sm")
 
-# Load the spaCy model
+# Load spaCy model
 nlp = load_spacy_model()
+
 
 
 # Load SentenceTransformer Embedding Model
